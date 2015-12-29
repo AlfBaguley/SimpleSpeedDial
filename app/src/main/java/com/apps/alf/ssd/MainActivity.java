610d631c13fc22d466d6358905779dee793aa16b
Alf Baguley <alf.baguley@gmail.com>
2015-12-29 19:02:17 +0000

    Added tiny bit to log.d "you shook me"

--- app/src/main/java/com/apps/alf/ssd/MainActivity.java
+++ app/src/main/java/com/apps/alf/ssd/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends ActionBarActivity implements OnInitListener {
 
                                                    if (!alreadyDoingOnShakeEvent) {
                                                        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-                                                       Log.d(DEBUGTAG, "YOU SHOOK ME");
+                                                       Log.d(DEBUGTAG, "YOU SHOOK ME PAL");
 
                                                        if (isConnected()) { // i.e. Is connected to the Internet
 
