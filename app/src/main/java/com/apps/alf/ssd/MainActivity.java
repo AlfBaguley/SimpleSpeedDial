5d12ff4394ddb17b2117412b9a0df195616d778d
Alf Baguley <alf.baguley@gmail.com>
2017-09-28 23:22:38 +0100

    Added more menus

--- app/src/main/java/com/apps/alf/ssd/MainActivity.java
+++ app/src/main/java/com/apps/alf/ssd/MainActivity.java
@@ -302,11 +302,32 @@ else {
     // ----------------------------------------------------------------------------------------------------------------------------
 
     @Override
+
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.action_settings:
+
+            {
+
+                // open new activity
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            }
+
+            case R.id.action_exit:
+
+            {
+                Toast.makeText(MainActivity.this, "Version 1.1", Toast.LENGTH_SHORT).show();
+            }
+
+        }
         if (id == R.id.action_settings) {
 
             // open new activity
@@ -314,6 +335,7 @@ else {
             startActivity(i);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
     // -----------------------------------------------------------------------------------------------------------------------------
